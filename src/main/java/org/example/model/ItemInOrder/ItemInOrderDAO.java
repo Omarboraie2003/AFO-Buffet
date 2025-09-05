87b7b7d5e551f8ed1e3b741b279ba24b62251ee6
sumoboi69 <120967839+sumoboi69@users.noreply.github.com>
2025-09-05 16:21:05 +0300

    item in order teaser trailer

---
+++ src/main/java/org/example/model/ItemInOrder/ItemInOrderDAO.java
@@ -0,0 +1,4 @@
+package org.example.model.ItemInOrder;
+
+public class ItemInOrderDAO {
+}
---
+++ src/main/java/org/example/model/ItemInOrder/ItemInOrderModel.java
@@ -0,0 +1,9 @@
+package org.example.model.ItemInOrder;
+
+public class ItemInOrderModel {
+    private int item_in_order_id;
+    private int item_id;
+    private String type_of_bread;
+    private String quantity;
+    private String note;
+}
